6717835654a4db3ab3e9d860f2cad2045f574ab0
Sarah <sarahlchavez@gmail.com>
2021-06-22 20:52:23 -0700

    commit tree 2c6829889be32ae516cdc8699aceb71587e1c378

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,12 @@
+class StaticController < ApplicationController
+
+    def hello_world
+        render "static/static_template"
+    end
+end
+
+class StaticController < ApplicationController
+    def about
+        #render "some_page"
+    end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get is the HTTP verb
+  # the path 'hellow_world' represents the path in the URL bar that the route will be mapped to
+  # the controller action 'static#hello_world' tells the Rails routing system that this route should be passed through the static controller's hello_world action
+  get 'hello_world', to: 'static#hello_world'
 end
